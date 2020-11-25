dfb0cc66a9e67e504893aade047b1609c1136b5d
Davi Rodrigues <developer.davi@gmail.com>
2020-11-25 11:00:34 -0300

    feat(routes): add pages

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Container from './Components/Container';
 import Header from './Components/Header';
 import HomePage from './Pages/HomePage';
+import MiningPage from './Pages/MiningPage';
+import StakingPage from './Pages/StakingPage';
 
 const Routes: React.FC = () => {
   return (
@@ -11,6 +13,8 @@ const Routes: React.FC = () => {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/staking" component={StakingPage} />
+          <Route exact path="/mining" component={MiningPage} />
         </Switch>
       </Container>
     </Router>
